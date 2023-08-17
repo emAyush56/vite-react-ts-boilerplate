879036a71155833994a7108bceb2904b4fa78b2f
Ayush Saha <searchme.ayush@gmail.com>
2023-08-17 15:45:45 +0530

    inital commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen w-full text-xl flex items-center justify-center bg-gray-200">
+      React/Vite/TypeSript/Tailwind/Herocions/ReactRouter/Prettier
+    </div>
+  );
+}
+
+export default App;
